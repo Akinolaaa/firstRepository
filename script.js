8c01c27d440d11b72b25cd799dcfe795a25c40a5
Akinola Akinleye <akin.akinleye619@gmail.com>
2020-05-22 16:06:20 +0100

    the npm libraries

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-var color1 = document.getElementById("color1");
-var color2 = document.getElementById("color2");
+var _ = require('lodash');
+
+var array = [1,2,3,4,5,6,7,8];
+console.log('answer:', _.without(array, 5));
+var color1 = document.querySelector("#color1");
+var color2 = document.querySelector("#color2");
 var css = document.querySelector("h3");
-var body = document.getElementById("gradient");
+var body = document.querySelector("#gradient");
 
 // console.log(css);
 // console.log(color1);
 // console.log(color2);
 function setGradient(){
 	//background: linear-gradient(to right, red, yellow);
-	body.style.background = "linear-gradient(to right," 
-		+ color1.value + ", "+ color2.value + ")";
+	body.style.background = `linear-gradient(to right,
+	 ${color1.value},  ${color2.value} )`;
 
 	css.innerHTML = body.style.background + ";" ;
 
@@ -17,7 +21,7 @@ function setGradient(){
 }
 
 color1.addEventListener("input", setGradient); 
-// onclick = "setGraient" --> an attribute of <input> could also 
+// onclick = "setGradient" --> an attribute of <input> could also 
 //                            be used but its not extensible 
 
 color2.addEventListener("input", setGradient);
